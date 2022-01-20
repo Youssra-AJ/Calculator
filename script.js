f275adfbcda416860426a65bc2a9c12480caa350
Youssra <ajattaryousra10@gmail.com>
2022-01-20 21:13:13 +0100

    constructed the addition function

---
+++ script.js
@@ -0,0 +1,65 @@
+//Start by writing functions for all the basic math 
+//operations: Add, Substract, Multiply, and Divide.
+//Test them with lots of console.log()!
+
+function add(num1, num2) {
+    return num1 + num2
+}
+    
+console.log(add(1, 3));
+
+
+
+
+
+
+
+//Create a function called 'operate' that takes an operator(?)
+//and 2 numbers and then calls one of the above functions
+//on the numbers. Meaning, this function 'operate' should
+//be able to either add/substract/multiply/divide any 2 
+//given numbers.
+
+
+
+
+//Now mving to HTML/CSS, create your calculator with 
+//HTML buttons for each digit, each operation (functions above),
+//and equals sign. Fill in the buttons with some light 
+//styling to get a picture of what you're building. 
+//Don't worry about the JS part of this.
+//Add "clear" button.
+
+
+
+//Create the functions that populate the display when
+//you click the number buttons. Meaning, you should 
+//make the functionality that displays whatever user 
+//clicks on. Add 'display value' as variable and store it
+//in global scope to use for next steps.
+
+
+
+
+//After you create the code that populates the display,
+//it's times to make the calculator do its job.
+//When the operate() function has been called, your
+//display should show the 'solution'. Stitch it all 
+//together.
+
+
+
+
+//Store all the values and call the operate() function
+//on them.
+
+
+
+
+//BUGS TO LOOK OUT FOR: 
+//Users should be able to string together several
+//operations and get the answer right.
+//Round answers that have long decimals cause we don't
+//want them to take up screen space.
+//Display an error message if the user tries to divide by
+//zero! Don't let it crush your calculator!
